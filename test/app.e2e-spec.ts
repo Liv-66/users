1a1366db593de4e0accabeba9a9979671f4e53a4
liv huang <liv@yunyun.cloud>
2023-08-18 21:44:46 +0800

    init project

---
+++ test/app.e2e-spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { UserModule } from './../src/user.module';
+
+describe('UserController (e2e)', () => {
+  let user: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [UserModule],
+    }).compile();
+
+    user = moduleFixture.createNestApplication();
+    await user.init();
+  });
+
+  it('/ (GET)', () => {
+    return request(user.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Hello World!');
+  });
+});
